b089f32ccccb2ae7a61171c3dcbc0040e3e78bed
Andres Canal <andresyo990@gmail.com>
2017-11-05 18:31:30 +0100

    created module for underline style

--- src/StyleEditor.js
+++ src/StyleEditor.js
@@ -2,44 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactQuill, { Quill, Mixin, Toolbar }  from 'react-quill';
 import CustomSnow from "./CustomSnow"
-
+import UnderlineStyle from "./custom-formats/UnderlineStyle.js"
 
 let Inline = Quill.import('blots/inline');
 
 var SizeStyle = Quill.import('attributors/style/size');
 SizeStyle.whitelist = ['13px','15px', '18px', '32px'];
-
-class UnderlineStyle extends Inline {
-  static create(underlineStyle) {
-    this.whitelist = [
-      "style-none",
-      "style-single",
-      "style-thick",
-      "style-double",
-      "pattern-dot",
-      "pattern-dash",
-      "pattern-dash-dot",
-      "pattern-Dash-dot-dot",
-      "by-word"
-    ];
-
-    let node = super.create();
-    node.setAttribute('class', underlineStyle);
-    return node;
-  }
-
-  static formats(node) {
-    let theClass = node.getAttribute('class');
-
-    if(this.whitelist.indexOf(theClass) > -1) {
-      return theClass
-    } else { 
-      return null
-    }
-  }
-}
-UnderlineStyle.blotName = 'underline-style';
-UnderlineStyle.tagName = 'u';
 
 Quill.register(UnderlineStyle)
 Quill.register(SizeStyle, true);
---
+++ src/custom-formats/UnderlineStyle.js
@@ -0,0 +1,36 @@
+import ReactQuill, { Quill, Mixin, Toolbar }  from 'react-quill';
+
+let Inline = Quill.import('blots/inline');
+
+class UnderlineStyle extends Inline {
+  static create(underlineStyle) {
+    this.whitelist = [
+      "style-single",
+      "style-thick",
+      "style-double",
+      "pattern-dot",
+      "pattern-dash",
+      "pattern-dash-dot",
+      "pattern-Dash-dot-dot",
+      "by-word"
+    ];
+
+    let node = super.create();
+    node.setAttribute('class', underlineStyle);
+    return node;
+  }
+
+  static formats(node) {
+    let theClass = node.getAttribute('class');
+
+    if(this.whitelist.indexOf(theClass) > -1) {
+      return theClass
+    } else { 
+      return null
+    }
+  }
+}
+UnderlineStyle.blotName = 'underline-style';
+UnderlineStyle.tagName = 'u';
+
+export default UnderlineStyle;
