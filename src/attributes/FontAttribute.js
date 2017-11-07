73e0e3a5a36a2cd1890bb32be80787290ee5f82f
Andres Canal <andresyo990@gmail.com>
2017-11-07 14:32:31 +0100

    for font attributes if size or name is provided for the font complete the missing one

--- src/attributes/FontAttribute.js
+++ src/attributes/FontAttribute.js
@@ -3,11 +3,21 @@ import Attribute from './Attribute';
 class FontAttribute extends Attribute {
   parse() {
     let attributes = this.attributes
+
+
+    if(attributes && "font" in attributes && !("size" in attributes)) {
+      attributes["size"] = "13px"
+    }
+
+    if(attributes && "size" in attributes && !("font" in attributes)) {
+      attributes["font"] = "helvetica-neue"
+    }
+
     if (attributes && "font" in attributes && "size" in attributes) {
       let fontName = this.valueFromClass(attributes["font"]);
       let fontSize = attributes["size"].replace("px", "");
 
-      let font = `UIFont(name: "${fontName}", size: ${fontSize})`;
+      let font = `UIFont(name: "${fontName}", size: ${fontSize})!`;
       return `NSAttributedStringKey.font: ${font}`;
     }
   }
