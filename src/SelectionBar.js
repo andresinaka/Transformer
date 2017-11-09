c9f87b10d62fc55a9b5492ab565a5a51c549a60b
Andres Canal <andresyo990@gmail.com>
2017-11-09 18:09:33 +0100

    changing from swift to obj c is working

--- src/SelectionBar.js
+++ src/SelectionBar.js
@@ -3,43 +3,49 @@ import ReactDOM from 'react-dom';
 
 class SelectionBar extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.objectiveCall = this.objectiveCall.bind(this);
+    this.swiftCall = this.swiftCall.bind(this);
+  }
+
+  objectiveCall() {
+    var objClickable = this.refs.ObjectiveClickable;
+    var swifClickable = this.refs.SwiftClickable;
+
+    swifClickable.classList.remove("selected-tab");
+    objClickable.classList.add("selected-tab");
+  }
+
+  swiftCall() {
+    var objClickable = this.refs.ObjectiveClickable;
+    var swifClickable = this.refs.SwiftClickable;
+
+    objClickable.classList.remove("selected-tab");
+    swifClickable.classList.add("selected-tab");
   }
 
   render() {
     return (
       <nav className="tabbar">
          <ul>
-            <li className="selected-tab">
+            <li className="selected-tab" ref="SwiftClickable">
                <div className="tab-contents swift-border-color">
-                  <span className="tab-name">Swift 4</span>
+                  <span className="tab-name" onClick={this.swiftCall}>Swift 4</span>
                </div>
             </li>
-            <li className="">
+            <li className="" ref="ObjectiveClickable">
                <div className="tab-contents objective-c-border-color ">
-                  <span className="tab-name">Objective-C</span>
+                  <span className="tab-name" onClick={this.objectiveCall}>Objective-C</span>
                </div>
             </li>
             <li className="full-opacity tab-name float-right">
               <div className="tab-contents copy-border-color">
                 <span className="tab-name" onClick={this.props.copyButtonClick}>Copy</span>
               </div>
-               
             </li>
-
          </ul>
       </nav>
-
-      // <div className = "col-12 nopadding">
-      //   <button type="button"
-      //           onClick={this.props.copyButtonClick}
-      //           className="float-right copy-button" >
-      //     <img src="images/copy.svg"
-      //          className ="copy-image"
-      //          width="13" 
-      //          alt="Copy to clipboard"/>
-      //   </button>
-      // </div>
     )
   }
 }
