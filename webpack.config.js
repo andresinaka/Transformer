b677a2a79bc8ffbab0350314a5f772011e76fb8a
Andres Canal <andresyo990@gmail.com>
2017-12-31 16:42:13 -0300

    using production build for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,63 @@
+var webpack = require('webpack');
 const path = require('path');
- 
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
-  ],
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      { 
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-      }
+
+module.exports = env => {
+
+  config = {
+    context: path.join(__dirname, 'src'),
+    entry: [
+      './main.js',
     ],
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-      {
-        test: /\.svg/,
-        use: {
-            loader: 'svg-url-loader',
-            options: {}
+    output: {
+      path: path.join(__dirname, 'www'),
+      filename: 'bundle.js',
+    },
+    module: {
+      loaders: [
+        { 
+          test: /\.css$/,
+          loader: "style-loader!css-loader"
+        },
+        {
+          test: /\.scss$/,
+          loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
         }
-      }
-    ],
-  },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
-};
+      ],
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+          ],
+        },
+        {
+          test: /\.svg/,
+          use: {
+              loader: 'svg-url-loader',
+              options: {}
+          }
+        }
+      ]
+    },
+    resolve: {
+      modules: [
+        path.join(__dirname, 'node_modules'),
+      ],
+    },
+    plugins: []
+  };
+
+  if (env.production) {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    );
+
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+  }
+  return config;
+}
